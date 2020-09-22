64eb5d9dd162fa1a2b22ddd6fa3c0cae83cf4800
TejasRattihalli <47137835+TejasRattihalli@users.noreply.github.com>
2020-09-22 17:52:57 +0530

    Updates

---
+++ precache-manifest.3397e7a8e82fdad7c5f0c47d7517fa16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e9ce06e58e7729d8f80799ffef7150",
+    "url": "/react11/index.html"
+  },
+  {
+    "revision": "c34be4d100c3cfd62784",
+    "url": "/react11/static/css/main.cd4f12a3.chunk.css"
+  },
+  {
+    "revision": "e627b9955492c1892196",
+    "url": "/react11/static/js/2.621ff91f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react11/static/js/2.621ff91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34be4d100c3cfd62784",
+    "url": "/react11/static/js/main.d8d3b280.chunk.js"
+  },
+  {
+    "revision": "2aaba147da8f330818f3",
+    "url": "/react11/static/js/runtime-main.f1fb9a1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react11/static/media/logo.5d5d9eef.svg"
+  }
+]);
